033451847db66c03a2cb14b30ce5fc3de016dedd
Rob <morrisrjc@gmail.com>
2021-01-28 06:33:16 +0000

    ajax functionality to dispay tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,11 +5,46 @@
  */
 
 $(document).ready(function () {
-renderTweets(data);
+  $(function() {
+    $.ajax({
+    url: "/tweets",
+    method: "GET",
+  })
+  .then((result) => {
+    renderTweets(result)
+    })
+  .catch((err) => {
+    console.log('ajax error caught');
+    console.log(err);
+  });
 
+$("form").on("submit", function (event) {
+  event.preventDefault();
+const $newtweet = $(".tweet-text")
+
+$.ajax({
+    url: "/tweets",
+    method: "POST",
+    data: $newtweet.serialize(),
+    success: () => {
+      $.ajax({
+        url: "/tweets",
+        method: "GET",
+        
+      })
+      .then((result) => {
+        renderTweets(result)
+        
+        .catch((err) => {
+          console.log('ajax error caught');
+          console.log(err); 
+          });
+        })
+        $(".tweet-text").val("")
+    }
+  })
+})
 });
-
-
 
 function createTweetElement(tweetObj) {
   const $markup =
@@ -23,7 +58,7 @@ function createTweetElement(tweetObj) {
       <div class= "userName">${tweetObj.user.handle}</div>
     </header>
     <body class= "article-body">
-      <p class= "tweet-paragraph">${tweetObj.content.text}</p>
+      <p class= "tweet-paragraph">${escape(tweetObj.content.text)}</p>
     </body>
     <footer class ="article-footer">${tweetObj.created_at}</footer>
   </article>
@@ -33,44 +68,42 @@ function createTweetElement(tweetObj) {
 
 const renderTweets = function(tweets) {
 // loops through tweets
-console.log("I'm in  function renderTweets");
 const container = $("#tweet-container");
-console.log(container);
   for (const tweet of tweets) {
-    console.log("building a tweet");
     let element = createTweetElement(tweet)
     container.prepend(element);
+    }
   }
-
+});
+ 
+const escape =  function(str) {
+  let div = document.createElement('div');
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
 }
 
 
 
 
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd"
-      },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
 
-  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ // $(document).ajaxSuccess(function(){
+  //   alert("Too may characters! please use 140 or less. save some space for others 🐤");
+  // });
+  // $("textarea").click(function(){
+  //   $("div").load("demo_ajax_load.txt");
+  // });
+//working pop up but on clicking tweet button
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
   console.log(" loading your jQuery!");
   $(".tweet-text").on("input ", function () {
-    let myLength = $(".tweet-text").val().length; 
-    $(".counter").text(140 - myLength);
-    if (myLength > 140) {
+    let charLength = $(".tweet-text").val().length;
+    $(".counter").text(140 - charLength);
+    if (charLength > 140) {
       $(".counter").removeClass("black").addClass("red");
     } else {
       $(".counter").removeClass("red").addClass("black");
